3b3a7522965ab01cfcf872151eb2ba00f4322ef4
apohies <miguelegion@gmail.com>
2022-03-22 09:46:52 -0500

    test aliance

---
+++ SocialMediaCore/DTOs/TestAlliance.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SocialMediaCore.DTOs
+{
+    class TestAlliance
+    {
+        public int MyProperty { get; set; }
+    }
+}
